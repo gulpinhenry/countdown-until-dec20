596d5988c9c3a2696ed1ca149d5f77f43672e9e8
Henry Kam <h3nrykam@gmail.com>
2021-10-11 20:19:24 -0700

    fun times LOL

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const dec20 = new Date(2021, 11, 20).getTime();
+const month = new Date().getMonth();
+const ashley = new Audio("./files/ashleyaudio.mp3");
+let timer = setInterval(function() {
+
+  const today = new Date().getTime();
+
+  let diff;
+  diff = dec20 - today;
+
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML =
+    "<div class=\"days\"> \
+  <div class=\"numbers\">" + days + "</div>days</div> \
+<div class=\"hours\"> \
+  <div class=\"numbers\">" + hours + "</div>hours</div> \
+<div class=\"minutes\"> \
+  <div class=\"numbers\">" + minutes + "</div>minutes</div> \
+<div class=\"seconds\"> \
+  <div class=\"numbers\">" + seconds + "</div>seconds</div> \
+</div>";
+
+}, 1000);
+
+let sound = setInterval(function(){
+    
+    ashley.play();
+}, 13000);
